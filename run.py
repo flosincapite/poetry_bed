01d57c7a38bd139c7bb8c01e28ffdcccab363615
cory <cory.massaro@gmail.com>
2020-07-27 12:57:56 +0000

    Adds ability to pickle embeddings.

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 import copy
+import pickle
 
 import keras
 from keras.utils.vis_utils import plot_model
@@ -34,6 +35,30 @@ class Frontend:
     def __init__(self, model_dir):
         self._model = keras.models.load_model(model_dir)
 
+    def pickle_embeddings(self, model_dir, symbols_txt, pickle_file):
+        model = keras.models.load_model(model_dir)
+        symbols = symbol_table.SymbolTable.read(symbols_txt)
+
+        # Creates a model that simply outputs embeddings. Layers 0, 2, 3 are
+        # the target word input, embedding, and reshape layers respectiely.
+        new_model = keras.models.Sequential()
+        for index in [0, 2, 3]:
+            new_model.add(model.layers[index])
+        
+        # Stores embeddings for all words in vocabulary as arrays of shape
+        # (embedding_dim, 1).
+        print('Calculating all embeddings ...')
+        embeddings = {}
+        for word, index in symbols.dictionary.items():
+            if not index % 100:
+                print(word, index)
+            target_vector = np.array([index])
+            prediction = new_model.predict(target_vector)
+            embeddings[word] = prediction.flatten()
+
+        with open(pickle_file, 'wb') as outp:
+            pickle.dump(embeddings, outp)
+
     def traverse(self, symbols_txt, source_word, target_word):
         symbols = symbol_table.SymbolTable.read(symbols_txt)
 
@@ -48,6 +73,8 @@ class Frontend:
         print('Calculating all embeddings ...')
         embeddings = {}
         for word, index in symbols.dictionary.items():
+            if not index % 100:
+                print(word, index)
             target_vector = np.array([index])
             prediction = new_model.predict(target_vector)
             embeddings[word] = prediction.flatten()
@@ -57,19 +84,19 @@ class Frontend:
         terms = []
         for i in range(100):
             print(i)
-            terms.append(most_similar_term(source))
+            terms.append(most_similar_term(source, embeddings))
             source += delta
         terms.append(target_word)
         print(' '.join(terms))
 
-    def plot(self, plot_png):
-        print(self._model)
-        self._model._layers = [
+    def plot(self, model_dir, plot_png):
+        model = keras.models.load_model(model_dir)
+        model._layers = [
                 layer
-                for layer in self._model._layers
+                for layer in model._layers
                 if not isinstance(layer, dict)]
         plot_model(
-                self._model, to_file=plot_png, show_shapes=True,
+                model, to_file=plot_png, show_shapes=True,
                 show_layer_names=True)
 
 
